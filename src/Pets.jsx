d2e6d16b6146d0e2ca421d30d9be9ea28ab7dd60
KhurramSagoo <34654560+KhurramSagoo@users.noreply.github.com>
2023-04-20 22:37:44 +0500

    pets

--- src/Pets.jsx
+++ src/Pets.jsx
@@ -2,11 +2,17 @@ import {useState} from 'react';
 
 export default function Pets (){
   const [cat, setCate]=useState(0);
+  const [pets, setPets]=useState({cats:0, dogs:0 })
+  console.log('pets:', Pets )
   return(
     <main>
       <div> cat : {cat}</div>
       <button onClick={()=>setCate(cat+1)}> +</button>
       <button onClick={()=>setCate(cat-1 )}>-</button>
+
+      <section>
+        <div > by object: </div>
+      </section>
     </main>
   )
 }
