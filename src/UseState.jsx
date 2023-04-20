3ae3ee6f861c4972b223f3166d1c32c3209615ef
KhurramSagoo <34654560+KhurramSagoo@users.noreply.github.com>
2023-04-20 22:04:38 +0500

    add muliply with 5

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react';
 
 export default function UseState() {
   const [state, setState] = useState(0);
+  const [product, setProduct] = useState(0);
   return (
     <section>
       <div> here is the count {state} </div>
       <button onClick={() => setState(state + 1)}>increment + </button>
-      <br/>
-      <hr/>
+      <br />
+      <hr />
       <button onClick={() => setState(state - 1)}>decrement -</button>
+      <div>here is the table of 5: {product}</div>
+      <button onClick={() => setProduct(product +5)}>
+        table count start
+      </button>
     </section>
   );
 }
